fe6620295ffb1e4341d4f4f0b444f380ce8c432d
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2024-03-19 18:20:51 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function binarySearch(list, element) {
+    if(list.length === 0){
+        return -1;
+    }
+    let front = 0;
+    let back = list.length - 1;
+
+    while (front <= back){
+        let mid = Math.floor((front + back) / 2)
+
+        if(list[mid] === element){
+            while(list[mid] === list[mid - 1]){
+                mid = mid -1;
+            }
+            return mid;
+        }
+        else if(list[mid] < element){
+            front = mid + 1;
+        }
+        else{
+            back = mid - 1;
+        }
+    }
     return -1;
 }
